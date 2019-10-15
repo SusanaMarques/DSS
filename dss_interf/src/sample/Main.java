6197b35fc31573bd8e52855045109ee1722108b0
Susana <susymarques99@gmail.com>
2019-10-15 18:09:23 +0100

    interf

--- dss_interf/src/sample/Main.java
+++ dss_interf/src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
 
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Stopify");
+        primaryStage.setScene(new Scene(root, 550, 550));
         primaryStage.show();
     }
 
