89b4c30e8ddaf4b3c4175837941da32539f62ad9
Susana <susymarques99@gmail.com>
2019-10-16 10:40:10 +0100

    interf

--- dss_interf/src/sample/Controller.java
+++ dss_interf/src/sample/Controller.java
@@ -1,32 +1,56 @@
 package sample;
 
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Node;
-import java.awt.event.ActionEvent;
-import java.io.IOException;
 
 
-public class Controller {
+public class Controller implements Initializable
+{
+    @FXML Pane apMain;
 
     @FXML
-    public void changeScreenButtonPushed(ActionEvent event) throws IOException {
+    private void handleButtonAction(ActionEvent event)
+    {
+        try
+        {
 
-        Parent loginParent = FXMLLoader.load(getClass().getResource("login.fxml"));
-        Scene loginScene = new Scene(loginParent);
+            Button tempButton = (Button)event.getSource();
+                    String fpath = "inic_" + tempButton.getId() + ".fxml";
+                    Parent inic_c_Parent = FXMLLoader.load(getClass().getResource(fpath));
+                    Scene inic_c_Scene = new Scene(inic_c_Parent);
 
 
-        //this line gets stage information
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(loginScene);
-        window.show();
+                    //this line gets stage information
+                    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    window.setScene(inic_c_Scene);
+                    window.show();
+
+            }
+
+        catch (IOException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+
+    }
 
 }
-
--- dss_interf/src/sample/Main.java
+++ dss_interf/src/sample/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-
+    @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sem_sessao_inic.fxml"));
         primaryStage.setTitle("Stopify");
