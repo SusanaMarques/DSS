925c6e42b63dd613800d89ab2f77a2d73063a38f
mariajbp <mariajbp00@gmail.com>
2019-12-05 14:54:51 +0000

    DOCUMENTATION IS UR FRIEND

--- media_center/src/Business/FormatoDesconhecidoException.java
+++ media_center/src/Business/FormatoDesconhecidoException.java
@@ -1,5 +1,8 @@
 package Business;
 
+/**
+ * Classe FormatoDesconhecidoException que devolve uma mensagem caso  o formato do conteúdo adicionado seja inválido
+ **/
 public class FormatoDesconhecidoException extends Exception {
     public FormatoDesconhecidoException(){
         super();
